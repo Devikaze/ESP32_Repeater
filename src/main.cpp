3ad1ea41389849760e0e1154321253e5423f81f6
Devikaze <83789517+Devikaze@users.noreply.github.com>
2023-08-25 14:51:09 +0300

    Redesgined the main

--- src/main.cpp
+++ src/main.cpp
@@ -20,69 +20,86 @@ const int LED2 = 27;
 bool led1State = false;
 bool led2State = false;
 
-void wifiScan(){
-if (WiFi.status() == WL_CONNECTED) {
-    return;
-  }
-  Serial.println("scan start");
-  // WiFi.scanNetworks will return the number of networks found
+void sendWiFiScanHtml() {
   int n = WiFi.scanNetworks();
 
-  Serial.println("scan done");
+  String response = R"(
+    <!DOCTYPE html><html>
+      <head>
+        <title>ESP32 Web Server Demo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+          html { font-family: sans-serif; text-align: center; }
+          body { display: inline-flex; flex-direction: column; }
+        </style>
 
+      </head>
+            
+      <body>
+        <h1>ESP32 Web Server</h1>
+
+        <table>
+            <tr>
+              <th>Index</th>
+              <th>SSID</th>
+              <th>RSSI</th>
+            </tr>
+            ROWS
+        </table> 
+
+        <form id="wifiForm">
+          <label>Which Wifi network would you like to choose?</label><br>
+          <input id="wifiChoice"><br>
+
+          <label>Input the wifi password</label><br>
+          <input id="wifiPassword"><br>
+
+          <button id="changeAction" type="submit" onclick="beforeSubmit();">Choose WiFi</button>
+        </form>
+
+
+
+        <script defer >
+            function beforeSubmit() {
+                var form = document.getElementById('wifiForm');
+                var wifiChoice = document.getElementById('wifiChoice').value;
+                var wifiPassword = document.getElementById('wifiPassword').value;
+                form.action="/wifi/" + wifiChoice + '/pass/' + wifiPassword;
+                console.log("called")
+              }
+        </script>
+      </body>
+    </html>
+    )";
+
+
+
+  String rows = "";
   if (n == 0) {
       Serial.println("no networks found");
   } else {
-    Serial.print(n);
-    Serial.println(" networks found");
     for (int i = 0; i < n; ++i) {
+      String rowTemplate = R"(
+      <tr>
+        <td>ROW_1</td>
+        <td>ROW_2</td>
+        <td>ROW_3</td>
+        <td>ROW_4</td>
+      </tr>)";
+
       // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      rowTemplate.replace("ROW_1", String(i+1));
+      rowTemplate.replace("ROW_2", WiFi.SSID(i));
+      rowTemplate.replace("ROW_3", String(WiFi.RSSI(i)));
+      rowTemplate.replace("ROW_4", (WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?"None":"Secure");
 
-      delay(10);
+      rows = rows + rowTemplate;
     }
   }
-  Serial.println("\nWhat Network would you like to join?");
 
-  while (Serial.available() == 0) {
-  }
-
-  int menuChoice = Serial.parseInt();
-
-  ssid_Wifi = WiFi.SSID(menuChoice - 1);
-
-  Serial.println(ssid_Wifi);
-
-  if(WiFi.encryptionType(menuChoice - 1) == 0){
-    password_Wifi = "";
-  }
-  else {
-    Serial.println("Please enter your password: ");
-    delay(5000);
-    while (Serial.available() > 0) {
-    password_Wifi = Serial.readStringUntil('\n');
-    }
-    Serial.println(password_Wifi);
-  }
-
-  WiFi.begin(ssid_Wifi,password_Wifi,6);
-  
-  Serial.print("Connecting to WiFi");
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  
-  Serial.println("\nConnected to WiFi");
+  response.replace("ROWS", rows);
+  server.send(200, "text/html", response);
 }
-
 void sendHtml() {
   String response = R"(
     <!DOCTYPE html><html>
@@ -105,67 +122,45 @@ void sendHtml() {
         <h1>ESP32 Web Server</h1>
 
         <div>
-          <h2>LED 1</h2>
-          <a href="/toggle/1" class="btn LED1_TEXT">LED1_TEXT</a>
-          <h2>LED 2</h2>
-          <a href="/toggle/2" class="btn LED2_TEXT">LED2_TEXT</a>
+          Connection successful
         </div>
       </body>
     </html>
   )";
-  response.replace("LED1_TEXT", led1State ? "ON" : "OFF");
-  response.replace("LED2_TEXT", led2State ? "ON" : "OFF");
   server.send(200, "text/html", response);
 }
 
 void setup(void) {
   Serial.begin(115200);
   WiFi.mode(WIFI_AP_STA);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-String password_AP = "12345678";
-String ssid_AP = "ESP32" ;
+  String password_AP = "12345678";
+  String ssid_AP = "ESP32" ;
   WiFi.softAP(ssid_AP,password_AP);
   Serial.println("Created AP");
+  Serial.print("ESP AP IP: ");
   Serial.println(WiFi.softAPIP());
+  
+  sendWiFiScanHtml();
 
-  wifiScan();
+  server.on("/", sendWiFiScanHtml);
 
-  Serial.print("Connecting to WiFi ");
-  Serial.print(ssid_Wifi);
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(100);
-    Serial.print(".");
-  }
-  Serial.println(" Connected!");
+  server.on(UriBraces("/wifi/{}/pass/{}"), []() {
+    String wifiIndex = server.pathArg(0);
+    String pass = server.pathArg(1);
 
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+    Serial.println(wifiIndex);
+    Serial.println(pass);
 
-  server.on("/", sendHtml);
-
-  server.on(UriBraces("/toggle/{}"), []() {
-    String led = server.pathArg(0);
-    Serial.print("Toggle LED #");
-    Serial.println(led);
-
-    switch (led.toInt()) {
-      case 1:
-        led1State = !led1State;
-        digitalWrite(LED1, led1State);
-        break;
-      case 2:
-        led2State = !led2State;
-        digitalWrite(LED2, led2State);
-        break;
-    }
+    WiFi.begin(WiFi.SSID(wifiIndex.toInt()-1),pass,6);
+    
+    Serial.println("WiFi Connected");
 
     sendHtml();
   });
-
   server.begin();
   Serial.println("HTTP server started");
+
+ 
 }
 
 void loop(void) {
